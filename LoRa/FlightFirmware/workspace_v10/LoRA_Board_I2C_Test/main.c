2c41b295a925426ca0322460599236eb463de3dd
Cameron Blegen <c.bleg98@gmail.com>
2021-02-17 00:31:05 -0700

    I2C rep. start (finally) working.
    Prepared now to move forward with gathering/packaging all I2C information.

--- LoRa/FlightFirmware/workspace_v10/LoRA_Board_I2C_Test/main.c
+++ LoRa/FlightFirmware/workspace_v10/LoRA_Board_I2C_Test/main.c
@@ -3,12 +3,17 @@
 //-----------------Globals-------------------
 int data_in = 0;
 int data_out = 0;
+int i;
+int data_rec = 0;
+int data_tran = 0;
 
 //------------------Functions---------------------------------------------------------------------
 void transmit_I2C(void){
+    UCB1CTLW0 |= UCTR;    // TX mode
+    data_out = 0x0A;
     UCB1CTLW0 |= UCTXSTT; // start condition
     while ((UCB1IFG & UCSTPIFG) == 0); // Wait for STOP
-        UCB1IFG &= ~UCSTPIFG; // Clear STOP Flag
+    UCB1IFG &= ~UCSTPIFG; // Clear STOP Flag
 }
 
 /**
@@ -28,10 +33,13 @@ int main(void)
     UCB1CTLW0 |= UCMODE_3;   // Put into I2C mode
     UCB1CTLW0 |= UCMST;      // Put into master mode
 
-    UCB1CTLW0 &= ~UCTR;    // RX mode
-    UCB1I2CSA = 0x4A;     // Slave address
-    UCB1CTLW1 |= UCASTP_2;   // Auto STOP when UCB1TBCNT reached
-    UCB1TBCNT = 0x1;      // Set characters to be send (1 byte)
+    //UCB1CTLW0 &= ~UCTR;    // RX mode
+    UCB1I2CSA = 0x4A;     // Slave address for temp sensor
+
+    //UCB1CTLW0 |= UCTR;    // TX mode
+    UCB1CTLW1 |= UCASTP_0;   // DO NOT Auto STOP when UCB1TBCNT reached
+    //UCB1CTLW1 |= UCASTP_2;   // Auto STOP when UCB1TBCNT reached
+    UCB1TBCNT = 0x2;      // Set characters to be send (1 byte)
 
     //------ 3. Configure Ports
     P4SEL1 &= ~BIT7;        // P4.7 is SCL
@@ -48,43 +56,65 @@ int main(void)
     UCB1IE |= UCTXIE; // Tx0 IRQ
     UCB1IE |= UCRXIE; // Rx0 IRQ
 
-    P4IFG &= ~BIT4; // Clear IRQ Flag
-    P4IFG &= ~BIT5; // Clear IRQ Flag
     P4IFG &= ~BIT6; // Clear IRQ Flag
     P4IFG &= ~BIT7; // Clear IRQ Flag
 
-    P1IE |= BIT4;   // Enable IRQ
-    P1IE |= BIT5;   // Enable IRQ
-    P1IE |= BIT6;   // Enable IRQ
-    P1IE |= BIT7;   // Enable IRQ
+    P4IE |= BIT6;   // Enable IRQ
+    P4IE |= BIT7;   // Enable IRQ
 
     __enable_interrupt(); // enable IRQs
 
     while(1){
 
+        /*//Pressure
+        UCB1I2CSA = 0x77;
+        UCB1TBCNT = 0x1;
         UCB1CTLW0 |= UCTR;    // TX mode
-        data_out = 0x0B; //get ID
+        data_out = 0x1E;
+        for(i = 0; i < 100; i++){}
         transmit_I2C();
-
+        data_out = 0x40;
+        for(i = 0; i < 100; i++){}
+        transmit_I2C();
+        data_out = 0x00;
+        for(i = 0; i < 100; i++){}
+        transmit_I2C();
+        UCB1TBCNT = 0x3;
         UCB1CTLW0 &= ~UCTR;    // RX mode
+        for(i = 0; i < 1500; i++){}
+        transmit_I2C();*/
+
+        //Temperature
         transmit_I2C();
+        data_rec = 0;
+        data_tran = 0;
     }
 
-	return 0;
+	//return 0;
 }
 
 #pragma vector=EUSCI_B1_VECTOR
 __interrupt void EUSCI_B1_I2C_ISR(void){
 
     switch(UCB1IV){
-    case 0x16:                  // ID 16: RXIFG1
-        data_in = UCB1RXBUF;
+    case 0x16:                  // ID 16: RX
+        if(data_rec == 0){
+            data_in = UCB1RXBUF;
+            data_rec = 1;
+        }
         break;
     case 0x18:                  // ID 18: TX
-        UCB1TXBUF = data_out;
+        if(data_tran == 0){
+            UCB1TXBUF = data_out;
+            data_tran = 1;
+        }else if(data_tran == 1){
+            UCB1CTLW0 &= ~UCTR;
+            UCB1CTLW0 |= UCTXSTT; // start condition
+            while((UCB1CTLW0 & UCTXSTT));
+            UCB1CTLW0 |= UCTXSTP; // stop condition
+        }
         break;
     default:
         break;
     }
-
 }
