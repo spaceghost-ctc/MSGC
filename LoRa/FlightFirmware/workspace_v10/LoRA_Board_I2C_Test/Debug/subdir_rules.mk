4fb8ee8eabe1f501305241d353d1b23017a63f8e
Cameron Blegen <c.bleg98@gmail.com>
2021-02-21 19:52:08 -0700

    I2C progress, pressure method working, getting wrong value

--- LoRa/FlightFirmware/workspace_v10/LoRA_Board_I2C_Test/Debug/subdir_rules.mk
+++ LoRa/FlightFirmware/workspace_v10/LoRA_Board_I2C_Test/Debug/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/bin/cl430" -vmspx --use_hw_mpy=F5 --include_path="C:/ti/ccs1020/ccs/ccs_base/msp430/include" --include_path="D:/GitHub/MSGC/LoRa/FlightFirmware/workspace_v10/LoRA_Board_I2C_Test" --include_path="C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include" --advice:power=all --advice:hw_config=all --define=__MSP430FR2355__ --define=_FRWP_ENABLE --define=_INFO_FRWP_ENABLE -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU40 --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/bin/cl430" -vmspx -Ooff --use_hw_mpy=F5 --include_path="C:/ti/ccs1020/ccs/ccs_base/msp430/include" --include_path="D:/GitHub/MSGC/LoRa/FlightFirmware/workspace_v10/LoRA_Board_I2C_Test" --include_path="C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include" --advice:power=all --advice:hw_config=all --define=__MSP430FR2355__ --define=_FRWP_ENABLE --define=_INFO_FRWP_ENABLE -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU40 --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
--- LoRa/FlightFirmware/workspace_v10/LoRA_Board_I2C_Test/main.c
+++ LoRa/FlightFirmware/workspace_v10/LoRA_Board_I2C_Test/main.c
@@ -1,20 +1,147 @@
 #include <msp430.h> 
 
+#define TEMP 0
+#define PRESSURE 1
+#define GET_PRESSURE_COEFFS 2
+#define ACCEL 3
+#define EXT_TEMP_ADDR 0x4A
+#define INT_TEMP_ADDR 0x4B
+#define RECEIVE 0
+#define SEND 1
+#define CONV_PRESSURE 0x48
+#define CONV_TEMP 0x58
+
 //-----------------Globals-------------------
-int data_in = 0;
-int data_out = 0;
-int i;
-int data_rec = 0;
-int data_tran = 0;
+short data_in = 0;
+short data_out = 0;
+unsigned int i;
+char I2C_sensor_case = TEMP; //0 = temp, 1 = pressure, 2 = get pressure coefficients, 3 = accelerometer
+char data_rec = 0;
+char data_tran = 0;
+char temp_I2C_flag = 0;
+char send_rec;
+char j;
+unsigned char crc;
+int temp_ext;
+int temp_int;
+long pressure = 0;
+long press_temp = 0;
+unsigned long digital_pressure_temp = 0;
+unsigned long digital_pressure = 0;
+//---pressure coefficients for calculation
+unsigned int coeffs[8]; // [blank, SENST1, OFFT1, TCS, TCO, TREF, TEMPSENSE, CRC bit]
 
 //------------------Functions---------------------------------------------------------------------
 void transmit_I2C(void){
-    UCB1CTLW0 |= UCTR;    // TX mode
-    data_out = 0x0A;
     UCB1CTLW0 |= UCTXSTT; // start condition
+    if((I2C_sensor_case == PRESSURE || I2C_sensor_case == GET_PRESSURE_COEFFS) && send_rec == RECEIVE){
+        UCB1IFG |= UCRXIFG0;
+    }
     while ((UCB1IFG & UCSTPIFG) == 0); // Wait for STOP
     UCB1IFG &= ~UCSTPIFG; // Clear STOP Flag
 }
+
+int get_temperature(char addr){
+
+    int temp = 0;
+    I2C_sensor_case = TEMP;
+    UCB1I2CSA = addr;
+    temp_I2C_flag = 1;
+    data_out = 0x00;
+    UCB1CTLW0 |= UCTR;    // TX mode
+    transmit_I2C();
+    temp = data_in;
+    temp = temp << 8;
+    data_out = 0x01;
+    data_rec = 0;
+    data_tran = 0;
+    UCB1CTLW0 |= UCTR;    // TX mode
+    transmit_I2C();
+    temp = temp + data_in;
+    data_rec = 0;
+    data_tran = 0;
+    temp_I2C_flag = 0;
+
+    return temp;
+}
+
+void read_pressure_sens(int data){
+    I2C_sensor_case = PRESSURE;
+    UCB1I2CSA = 0x77;
+    UCB1CTLW0 |= UCTR;    // TX mode
+
+    send_rec = SEND;
+
+    data_out = data;
+    transmit_I2C();
+    for(i = 0; i < 900; i++){}
+    data_tran = 0;
+
+    data_out = 0x00;
+    transmit_I2C();
+    for(i = 0; i < 200; i++){}
+    data_tran = 0;
+
+    send_rec = RECEIVE;
+
+    //UCB1TBCNT = 0x3; //set characters to recieve (3 bytes)
+    digital_pressure_temp = 0;
+    UCB1CTLW0 &= ~UCTR;    // RX mode
+    transmit_I2C();
+    data_rec = 0;
+    for(i = 0; i < 100; i++){}
+
+}
+
+void get_pressure_coeff(int coeff){
+    I2C_sensor_case = PRESSURE;
+    data_out = coeff;
+    send_rec = SEND;
+    UCB1CTLW0 |= UCTR;    // TX mode
+    transmit_I2C();
+    data_tran = 0;
+    for(i = 0; i < 500; i++){}
+
+    I2C_sensor_case = GET_PRESSURE_COEFFS;
+    if(coeff != 0xA2 || temp_I2C_flag == 1){
+        send_rec = RECEIVE;
+        temp_I2C_flag = 1;
+    }
+    UCB1CTLW0 &= ~UCTR;    // RX mode
+    transmit_I2C();
+    data_rec = 0;
+    for(i = 0; i < 500; i++){}
+}
+
+void reset_pressure(void){
+    I2C_sensor_case = PRESSURE;
+    UCB1I2CSA = 0x77;
+    UCB1CTLW0 |= UCTR;    // TX mode
+
+    send_rec = SEND;
+
+    data_out = 0x1E;
+    for(i = 0; i < 500; i++){}
+    transmit_I2C();
+    data_tran = 0;
+    for(i = 0; i < 350; i++){}
+
+    for(j = 0; j < 8; j++){
+    get_pressure_coeff((0xA0+j*2));
+    coeffs[j] = data_in;
+    }
+}
+
+void calculate_pressure(void){
+    long dT;
+    long OFF;
+    long SENS;
+    long temp;
+
+
+
+}
+
 
 /**
  * main.c
@@ -22,7 +149,23 @@ void transmit_I2C(void){
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
+
+    PM5CTL0 &= ~LOCKLPM5; // Turn on GPIO
+
+    P4SEL1 &= ~BIT7;        // P4.7 is GPIO
+    P4SEL0 &= ~BIT7;         // ...
+    //P4SEL1 &= ~BIT6;        // P4.6 is GPIO
+    //P4SEL0 &= ~BIT6;         // ...
+
+    P4DIR |= BIT7;          //Temporarily set 7 to output
+    P4OUT &= ~BIT7;          // set P2.6 pin to high as to clear clock on reset
+    //P4DIR |= BIT6;          //Temporarily set 6 to output
+    //P4OUT |= BIT6;          // set P2.7 pin to high as to clear data on reset
+
+    for(i = 0; i < 200; i++){}
+
+    PM5CTL0 |= LOCKLPM5; // Turn on GPIO
+
 //------------------Configure I2C Master Mode---------------------------------------------------------------------
 	//------ 1. Software Reset
 	UCB1CTLW0 |= UCSWRST;   // UCSWRST = 1 for SW reset
@@ -34,20 +177,18 @@ int main(void)
     UCB1CTLW0 |= UCMST;      // Put into master mode
 
     //UCB1CTLW0 &= ~UCTR;    // RX mode
-    UCB1I2CSA = 0x4A;     // Slave address for temp sensor
+    //UCB1I2CSA = 0x4A;     // Slave address for temp sensor
 
     //UCB1CTLW0 |= UCTR;    // TX mode
     UCB1CTLW1 |= UCASTP_0;   // DO NOT Auto STOP when UCB1TBCNT reached
     //UCB1CTLW1 |= UCASTP_2;   // Auto STOP when UCB1TBCNT reached
-    UCB1TBCNT = 0x2;      // Set characters to be send (1 byte)
+    //UCB1TBCNT = 0x3;      // Set characters to be send (1 byte)
 
     //------ 3. Configure Ports
     P4SEL1 &= ~BIT7;        // P4.7 is SCL
     P4SEL0 |= BIT7;
     P4SEL1 &= ~BIT6;        // P4.6 is SDA
     P4SEL0 |= BIT6;
-
-    PM5CTL0 &= ~LOCKLPM5; // Turn on GPIO
 
     //------ 4. Take out of SW reset
     UCB1CTLW0 &= ~UCSWRST;
@@ -64,30 +205,21 @@ int main(void)
 
     __enable_interrupt(); // enable IRQs
 
+    reset_pressure();
+
     while(1){
 
-        /*//Pressure
-        UCB1I2CSA = 0x77;
-        UCB1TBCNT = 0x1;
-        UCB1CTLW0 |= UCTR;    // TX mode
-        data_out = 0x1E;
-        for(i = 0; i < 100; i++){}
-        transmit_I2C();
-        data_out = 0x40;
-        for(i = 0; i < 100; i++){}
-        transmit_I2C();
-        data_out = 0x00;
-        for(i = 0; i < 100; i++){}
-        transmit_I2C();
-        UCB1TBCNT = 0x3;
-        UCB1CTLW0 &= ~UCTR;    // RX mode
-        for(i = 0; i < 1500; i++){}
-        transmit_I2C();*/
-
         //Temperature
-        transmit_I2C();
-        data_rec = 0;
-        data_tran = 0;
+        //temp_ext = get_temperature(EXT_TEMP_ADDR);
+        //temp_int = get_temperature(INT_TEMP_ADDR);
+
+        //Pressure
+        read_pressure_sens(CONV_PRESSURE);
+        digital_pressure = (digital_pressure_temp & 0x00FFFFFF);
+        read_pressure_sens(CONV_TEMP);
+        digital_pressure_temp = (digital_pressure_temp & 0x00FFFFFF);
+        calculate_pressure();
+        for(i = 0; i < 10; i++){}
     }
 
 	//return 0;
@@ -98,23 +230,68 @@ __interrupt void EUSCI_B1_I2C_ISR(void){
 
     switch(UCB1IV){
     case 0x16:                  // ID 16: RX
-        if(data_rec == 0){
-            data_in = UCB1RXBUF;
-            data_rec = 1;
+        switch(I2C_sensor_case){
+        case TEMP:
+            if(data_rec == 0){
+                data_in = UCB1RXBUF;
+                data_rec = 1;
+            }
+            break;
+        case PRESSURE:
+            if(data_rec < 3){
+                digital_pressure_temp = digital_pressure_temp + UCB1RXBUF;
+                digital_pressure_temp = digital_pressure_temp << 8;
+                data_rec++;
+            }else if(data_rec == 3){
+                digital_pressure_temp = digital_pressure_temp + UCB1RXBUF;
+                data_rec++;
+                UCB1CTLW0 |= UCTXSTP; // stop condition
+            }
+            break;
+        case GET_PRESSURE_COEFFS:
+            if(data_rec == 0){
+                data_in = UCB1RXBUF;
+                data_in = data_in << 8;
+                data_rec++;
+            }else if(data_rec == 1){
+                data_in = data_in + UCB1RXBUF;
+                data_rec++;
+                UCB1CTLW0 |= UCTXSTP; // stop condition
+            }
+            break;
+        default:
+            break;
         }
         break;
+
     case 0x18:                  // ID 18: TX
-        if(data_tran == 0){
-            UCB1TXBUF = data_out;
-            data_tran = 1;
-        }else if(data_tran == 1){
-            UCB1CTLW0 &= ~UCTR;
-            UCB1CTLW0 |= UCTXSTT; // start condition
-            while((UCB1CTLW0 & UCTXSTT));
-            UCB1CTLW0 |= UCTXSTP; // stop condition
+        switch(I2C_sensor_case){
+        case TEMP:
+            if(data_tran == 0){
+                UCB1TXBUF = data_out;
+                data_tran = 1;
+            }else if(data_tran == 1){
+                UCB1CTLW0 &= ~UCTR;
+                UCB1CTLW0 |= UCTXSTT; // start condition
+                while((UCB1CTLW0 & UCTXSTT));
+                UCB1CTLW0 |= UCTXSTP; // stop condition
+            }
+            break;
+        case PRESSURE:
+            if(data_tran == 0){
+                UCB1TXBUF = data_out;
+                data_tran = 1;
+            }else if(data_tran == 1){
+                UCB1CTLW0 |= UCTXSTP; // stop condition
+            }
+            break;
+        default:
+            break;
         }
         break;
+
     default:
         break;
+
     }
 }
