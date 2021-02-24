abee379af66b6340537e687cc0fcb2ed694d5759
Cameron Blegen <c.bleg98@gmail.com>
2021-02-23 23:02:28 -0700

    I2C WORKING FOR ALL SENSORS

--- LoRa/FlightFirmware/workspace_v10/LoRaMain/Debug/inst.d
+++ LoRa/FlightFirmware/workspace_v10/LoRaMain/Debug/inst.d
@@ -1,39 +1,39 @@
 # FIXED
 
 inst.obj: ../inst.c
-inst.obj: C:/ti/ccs1011/ccs/ccs_base/msp430/include/msp430.h
-inst.obj: C:/ti/ccs1011/ccs/ccs_base/msp430/include/msp430fr2355.h
-inst.obj: C:/ti/ccs1011/ccs/ccs_base/msp430/include/in430.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics_legacy_undefs.h
-inst.obj: C:/ti/ccs1011/ccs/ccs_base/msp430/include/legacy.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/stdint.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_ti_config.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/linkage.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_stdint40.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/stdint.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/cdefs.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/_types.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_types.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_stdint.h
-inst.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/_stdint.h
+inst.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h
+inst.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430fr2355.h
+inst.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics_legacy_undefs.h
+inst.obj: C:/ti/ccs1020/ccs/ccs_base/msp430/include/legacy.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/stdint.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/_ti_config.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/linkage.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/_stdint40.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/sys/stdint.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/sys/cdefs.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/sys/_types.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/machine/_types.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/machine/_stdint.h
+inst.obj: C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/sys/_stdint.h
 inst.obj: ../inst.h
 
 ../inst.c: 
-C:/ti/ccs1011/ccs/ccs_base/msp430/include/msp430.h: 
-C:/ti/ccs1011/ccs/ccs_base/msp430/include/msp430fr2355.h: 
-C:/ti/ccs1011/ccs/ccs_base/msp430/include/in430.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics_legacy_undefs.h: 
-C:/ti/ccs1011/ccs/ccs_base/msp430/include/legacy.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/stdint.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_ti_config.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/linkage.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_stdint40.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/stdint.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/cdefs.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/_types.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_types.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_stdint.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/_stdint.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/msp430fr2355.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccs1020/ccs/ccs_base/msp430/include/legacy.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/_ti_config.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/linkage.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/_stdint40.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/sys/stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/sys/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/machine/_types.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/machine/_stdint.h: 
+C:/ti/ccs1020/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include/sys/_stdint.h: 
 ../inst.h: 
